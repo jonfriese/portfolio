e363b4105661d86e2a3c2ca4b8dce705403f9657
noj33 <jonfriese33@gmail.com>
2013-10-10 14:35:21 -0700

    fixes delete test

--- test/features/post_delete_test.rb
+++ test/features/post_delete_test.rb
@@ -2,12 +2,18 @@ require "test_helper"
 
 feature "Deleting a Post" do
   scenario "post is deleted with a click" do
-    
-    visit posts_path
-    
-    page.find("tr:last").click_on "Destroy"
-    
-    page.wont_have_content "Code Rails"
+
+  @only = post(:cr).id
+
+  #Given I have a existing post
+  visit posts_path
+
+  #When I click the delete link
+  puts "/posts/#{@only}"
+  click_link('Destroy', href: "/posts/#{@only}")
+
+  #Then post is destroyed and no longer seen
+  page.wont_have_content post(:cr).title
+
   end
 end
-
